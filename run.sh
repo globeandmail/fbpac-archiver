79d02dc6da59787944566f526335dc79e3e1ae89
Steve Mickeler <smickeler@C02Y46CRJG5J-MBP.globeandmail.net>
2019-08-23 12:12:05 -0400

    copy most recently uploaded file to latest

--- run.sh
+++ run.sh
@@ -8,9 +8,11 @@ if [[ -z $DATABASE_URL || -z $ARCHIVE_BUCKET ]]; then
     exit 1
 fi
 
-echo "starting postgres export" &&
+echo "$(date -u) starting postgres export" &&
     psql "${DATABASE_URL}" -c "\COPY (SELECT * FROM ads WHERE (paid_for_by is not null or political_probability > 0.7) AND suppressed=false) to '${FILENAME}.csv' with csv header" &&
-    echo "compressing ${FILENAME}.csv to ${FILENAME}.tar.gz" &&
+    echo "$(date -u) compressing ${FILENAME}.csv to ${FILENAME}.tar.gz" &&
     tar -cvzf "${FILENAME}.tar.gz" "${FILENAME}.csv" &&
-    echo "copying ${FILENAME}.tar.gz to s3://${ARCHIVE_BUCKET}" &&
-    aws s3 cp --acl public-read "${FILENAME}.tar.gz" s3://${ARCHIVE_BUCKET}/
+    echo "$(date -u) copying ${FILENAME}.tar.gz to s3://${ARCHIVE_BUCKET}" &&
+    aws s3 cp --acl public-read "${FILENAME}.tar.gz" s3://${ARCHIVE_BUCKET}/ &&
+    echo "$(date -u) copying s3://${ARCHIVE_BUCKET}/${FILENAME}.tar.gz to s3://${ARCHIVE_BUCKET}/fbpac-archive-latest.tar.gz" &&
+    aws s3 cp --acl public-read s3://${ARCHIVE_BUCKET}/${FILENAME}.tar.gz s3://${ARCHIVE_BUCKET}/fbpac-archive-latest.tar.gz
