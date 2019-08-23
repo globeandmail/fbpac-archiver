4a5bca82b4df82d56a886b843cdd521c0158df65
Steve Mickeler <smickeler@C02Y46CRJG5J-MBP.globeandmail.net>
2019-08-23 10:54:41 -0400

    add some verbosity to run.sh output

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@ if [[ -z $DATABASE_URL || -z $ARCHIVE_BUCKET ]]; then
     exit 1
 fi
 
-psql "${DATABASE_URL}" -c "\COPY (SELECT * FROM ads WHERE (paid_for_by is not null or political_probability > 0.7) AND suppressed=false) to '${FILENAME}.csv' with csv header" &&
+echo "starting postgres export" &&
+    psql "${DATABASE_URL}" -c "\COPY (SELECT * FROM ads WHERE (paid_for_by is not null or political_probability > 0.7) AND suppressed=false) to '${FILENAME}.csv' with csv header" &&
+    echo "compressing ${FILENAME}.csv to ${FILENAME}.tar.gz" &&
     tar -cvzf "${FILENAME}.tar.gz" "${FILENAME}.csv" &&
-    aws s3 cp --acl public-read "${FILENAME}.tar.gz" s3://${ARCHIVE_BUCKET}/ &&
-    rm "${FILENAME}.csv" "${FILENAME}.tar.gz"
+    echo "copying ${FILENAME}.tar.gz to s3://${ARCHIVE_BUCKET}" &&
+    aws s3 cp --acl public-read "${FILENAME}.tar.gz" s3://${ARCHIVE_BUCKET}/
